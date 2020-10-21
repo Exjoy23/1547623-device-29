fff1bd4b88219170da1466c837bbe205ed77f46f
Alexey Lazarenko <70312230+Exjoy23@users.noreply.github.com>
2020-10-21 23:08:07 +0300

    добавил 2-й слайдер, партнеров и блок с инфой

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 'use strict';
 
 function showSlide(selector) {
-    selector.classList.remove('visually__hidden');
-  }
-  
-  function hideSlide(selector) {
-    selector.classList.add('visually__hidden');
-  }
-  
-  function addCurrent(selector) {
-    selector.classList.add('current');
-  }
-  
-  function removeCurrent(selector) {
-    selector.classList.remove('current');
-  }
+  selector.classList.remove('visually__hidden');
+}
+
+function hideSlide(selector) {
+  selector.classList.add('visually__hidden');
+}
+
+function addCurrent(selector) {
+  selector.classList.add('current');
+}
+
+function removeCurrent(selector) {
+  selector.classList.remove('current');
+}
 
 // GALLERY SLIDER
 
@@ -23,7 +23,8 @@ const gallerySlides = gallery.querySelectorAll('.gallery__slider__item');
 const galleryButtons = gallery.querySelectorAll('.gallery__slider__control__button');
 
 galleryButtons.forEach((item, i) => {
-  item.addEventListener('click', () => {
+  item.addEventListener('click', (evt) => {
+    evt.preventDefault();
     galleryButtons.forEach(item => {
       removeCurrent(item);
       gallerySlides.forEach(item => {
@@ -32,5 +33,25 @@ galleryButtons.forEach((item, i) => {
     });
     addCurrent(item);
     showSlide(gallerySlides[i]);
+  });
+});
+
+// SERVICES SLIDER
+
+const services = document.querySelector('.services');
+const servicesSlides = services.querySelectorAll('.services__item');
+const servicesButtons = services.querySelectorAll('.services__list__control__link');
+
+servicesButtons.forEach((item, i) => {
+  item.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    servicesButtons.forEach(item => {
+      removeCurrent(item);
+      servicesSlides.forEach(item => {
+        hideSlide(item);
+      });
+    });
+    addCurrent(item);
+    showSlide(servicesSlides[i]);
   });
 });
