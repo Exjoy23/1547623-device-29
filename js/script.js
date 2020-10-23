cbff6375966a9130ac12bdf516000e22f0c2d4eb
Alexey Lazarenko <70312230+Exjoy23@users.noreply.github.com>
2020-10-23 23:34:05 +0300

    добавил попап

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
 'use strict';
 
-function showSlide(selector) {
-  selector.classList.remove('visually__hidden');
+// FUNCTIONS
+
+function createSlider(item, i, buttons, slides) {
+  item.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    buttons.forEach(item => {
+      item.classList.remove('current');
+      slides.forEach(item => {
+        item.classList.add('visually__hidden');
+      });
+    });
+    item.classList.add('current');
+    slides[i].classList.remove('visually__hidden');
+  });
 }
 
-function hideSlide(selector) {
-  selector.classList.add('visually__hidden');
+function openModal(modal) {
+  modal.classList.add('current');
+  modal.classList.add('show');
 }
 
-function addCurrent(selector) {
-  selector.classList.add('current');
+function closeModal(modal) {
+  modal.classList.add('hide');
+  removeClassOnTimer(modal, 'current', 1000);
+  removeClassOnTimer(modal, 'show', 1000);
+  removeClassOnTimer(modal, 'hide', 1000);
 }
 
-function removeCurrent(selector) {
-  selector.classList.remove('current');
+function removeClassOnTimer(selector, addClass, time) {
+  setTimeout(() => selector.classList.remove(addClass), time);
 }
 
 // GALLERY SLIDER
@@ -23,17 +39,7 @@ const gallerySlides = gallery.querySelectorAll('.gallery__slider__item');
 const galleryButtons = gallery.querySelectorAll('.gallery__slider__control__button');
 
 galleryButtons.forEach((item, i) => {
-  item.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    galleryButtons.forEach(item => {
-      removeCurrent(item);
-      gallerySlides.forEach(item => {
-        hideSlide(item);
-      });
-    });
-    addCurrent(item);
-    showSlide(gallerySlides[i]);
-  });
+  createSlider(item, i, galleryButtons, gallerySlides);
 });
 
 // SERVICES SLIDER
@@ -43,15 +49,38 @@ const servicesSlides = services.querySelectorAll('.services__item');
 const servicesButtons = services.querySelectorAll('.services__list__control__link');
 
 servicesButtons.forEach((item, i) => {
-  item.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    servicesButtons.forEach(item => {
-      removeCurrent(item);
-      servicesSlides.forEach(item => {
-        hideSlide(item);
-      });
-    });
-    addCurrent(item);
-    showSlide(servicesSlides[i]);
-  });
+  createSlider(item, i, servicesButtons, servicesSlides);
+});
+
+// MAP
+
+const info = document.querySelector('.info');
+const openMap = info.querySelector('.contacts__link__map');
+const closeMap = info.querySelector('.contacts__map__button');
+const map = info.querySelector('.contacts__map');
+
+openMap.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  openModal(map);
+});
+
+closeMap.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  closeModal(map);
+});
+
+// POPUP
+
+const openPopup = info.querySelector('.contacts__link');
+const closePopup = info.querySelector('.contacts__popup__button');
+const popup = info.querySelector('.contacts__popup');
+
+openPopup.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  openModal(popup);
+});
+
+closePopup.addEventListener('click', (evt) => {
+  evt.preventDefault();
+  closeModal(popup);
 });
